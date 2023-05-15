7190f8f25fdcbdd434b092b18df70265b180ef55
Teodor Alatas <89796321+Trickster0-cmd@users.noreply.github.com>
2023-05-15 17:13:58 +0800

    init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
     <link rel="icon" href="https://slc-sflu.edu.ph/images/a.png" type="image/png">
 
     <!-- Scripts -->
-    <script src="https://raw.githubusercontent.com/Trickster0-cmd/alatas-vercel-laravel/main/public/js/app.js" defer></script>
+    <script src="{{ url('js/app.js') }}"></script>
+
 
 
     
